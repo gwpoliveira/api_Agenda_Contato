692613ac63bdb564faa53e03c6e541afc5335d19
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-05-08 18:53:51 -0300

    atualizado
    segue sim

--- contatos/urls.py
+++ contatos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contatos/', views.ContatoListCreate.as_view(), name='contatos-list-create'),
+    path('', views.ContatoListCreate.as_view(), name='contatos-list-create'),
     path('contatos/<int:pk>/', views.ContatoRetrieveUpdateDestroy.as_view(), name='contatos-detail'),
 ]
